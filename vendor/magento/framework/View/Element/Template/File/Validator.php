5d52f458d525c4f2c21e8286a0f184ecce40c6b2
Dmitry Fedyuk <dfediuk@gmail.com>
2022-01-17 17:02:50 +0600

    no message

--- vendor/magento/framework/View/Element/Template/File/Validator.php
+++ vendor/magento/framework/View/Element/Template/File/Validator.php
@@ -139,8 +139,12 @@ class Validator
             $directories = (array)$directories;
         }
         $realPath = $this->fileDriver->getRealPath($path);
-        foreach ($directories as $directory) {
-            if (0 === strpos($realPath, $directory)) {
+		# 2022-01-17 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+		# "How to fix the «Invalid template file» / «require_js.phtml» failure of Magento ≥ 2.3 in Windows?":
+		# https://mage2.pro/t/5774
+		$isWin = 'WIN' === strtoupper(substr(PHP_OS, 0, 3)); /** @var bool $isWin */
+		foreach ($directories as $directory) {
+			if (0 === strpos($realPath, !$isWin ? $directory : str_replace('/', DIRECTORY_SEPARATOR, $directory))) {
                 return true;
             }
         }
